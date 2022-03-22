55d7bd8b2b5e2ab03ee1ddb8c4577fd123874b69
aymannajim <36258300+notaymanTV@users.noreply.github.com>
2022-03-22 17:10:55 +0100

    Fixed ENTER button

--- html/main.js
+++ html/main.js
@@ -69,6 +69,8 @@ $(document).keyup(function (event) {
 		$('#container').hide();
 		currentMenuAction = null
 		return
+	} else if (event.which === 13) {
+		$( "#butt" ).trigger( "click" );
 	}
 });
 
